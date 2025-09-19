4a2adf7d743e03f41c09c533a0a32ed89aee842f
David Miyabe <davidmiyabe@users.noreply.github.com>
2025-09-18 19:12:47 -0700

    Add Streamlit trip planning wizard and itinerary view

---
+++ meguru/ui/__init__.py
@@ -0,0 +1,9 @@
+"""Meguru Streamlit UI helpers."""
+
+from .plan import ensure_plan_state, render_itinerary_tab, render_plan_tab
+
+__all__ = [
+    "ensure_plan_state",
+    "render_itinerary_tab",
+    "render_plan_tab",
+]
